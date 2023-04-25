0d66c22b127695e7f0807d356e77eedeadc13468
Alex <115658508+Alexlimoeses@users.noreply.github.com>
2023-04-25 00:47:17 -0300

    Atualizado com conversor, gráficos e tesouro selic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from bcb import currency
 import matplotlib.pyplot as plt
 from tkcalendar import DateEntry
@@ -5,54 +6,99 @@ from datetime import datetime, timedelta
 from tkinter import *
 from tkinter import ttk
 import requests
-
+from forex_python.converter import CurrencyRates
+from tkinter import Tk, PhotoImage
+conversor = CurrencyRates()
 
 class conversor_moeda:
     def __init__(self,master):
         #Criando a janela
-        self.master = master
+        self.master = master 
         
         # Definindo dimensoes da janela
-        master.geometry('500x400')
-        self.master.title('Conversor de Moedas')
-
-        # Criando as labels
-        self.moeda_label = Label(self.master, text="Selecione a moeda:")
-        self.dat = Label(self.master, text="Escolha a data para a plotagem do gráfico:")
-
-        # Definir as opções para o ComboBox
-        self.moedas = ['Dólar (USD)', 'Euro (EUR)', 'Libra (GBP)', 'Dolar canadense (CAD)']
+        master.geometry('340x260+300+100')
+        master.title('Conversor de Moedas')
+        master.resizable(width=0, height=0)
+      
+        #Criado as abas
+        self.notebook = ttk.Notebook(master)
+        self.notebook.place(x=6, y=0, width=330, height=255)
         
-        self.moedas_con = ['Dólar (USD)', 'Euro (EUR)', 'Libra (GBP)', 'Dolar canadense (CAD), Real(BRL)']
-        self.moedas_con2 = ['Dólar (USD)', 'Euro (EUR)', 'Libra (GBP)', 'Dolar canadense (CAD), Real(BRL)']
+        self.conv = Frame(master, bg='#00BFFF', borderwidth=2, relief='sunken')
+        self.grafic = Frame(master,bg='#6A5ACB', borderwidth=2, relief='sunken')
+        self.selic = Frame(master,bg='#0000FF', borderwidth=2, relief='sunken')
+        
+        # Adicione as abas ao notebook
+        self.notebook.add(self.grafic, text='Gráfico')
+        self.notebook.add(self.conv, text='Conversor')
+        self.notebook.add(self.selic, text='Selic')
+        
+        # Criando a entradas
+        self.dat = Label(self.grafic, bg='#6A5ACB',foreground='white', text="Escolha a Moeda desejada para plotagem do gráfico:")
+        self.dat.place(x=0, y=80)
+        
+        self.label_dat = Label(self.grafic, bg='#6A5ACB',foreground='white', text="Escolha a data inicial para plotagem do gráfico de variação:")
+        self.label_dat.place(x=0, y=0)
+    
+        self.cal = DateEntry(self.grafic, bg='#0000FF',foreground='white', width=8, background='darkblue',  borderwidth=2)
+        self.cal.place(x=0, y=25)
+        
+        # Criando o botão de plotar gráfico
+        self.plotar_grafico_button = Button(self.grafic, bg='#808080', foreground='white', text="Plotar Gráfico", command=self.gerar_grafico)
+        self.plotar_grafico_button.place(x=90,y=170)
+        
+        # Definir as opções para o ComboBox
+        self.moedas = ['Dólar (USD)', 'Euro (EUR)', 'Libra (GBP)', 'Dolar canadense (CAD)']       
+        self.moedas_con = ['USD', 'EUR', 'GBP', 'CAD', 'BRL']
+        self.moedas_con2 = ['USD', 'EUR', 'GBP', 'CAD', 'BRL']
 
         # Criar o ComboBox
-        self.combo_moedas = ttk.Combobox(self.master, values=self.moedas)
-        self.destin = ttk.Combobox(self.master, values=self.moedas_con, width=31)
-        self.fin = ttk.Combobox(self.master, values=self.moedas_con2, width=31)
-
-        # Definir o valor padrão
-        self.combo_moedas.set('Selecione uma moeda')
-        self.destin.set('Selecione a moeda a ser convertida')
-        self.fin.set('Selecione a sua moeda final')
-
-        # Criando o botão de plotar gráfico
-        self.plotar_grafico_button = Button(self.master, text="Plotar Gráfico", command=self.gerar_grafico)
-        self.converss = Button(self.master, text='Converter', command=self.convo)
         
-        #Criar entrada para data
-        self.cal = DateEntry(master, width=12, background='darkblue', foreground='white', borderwidth=2)
-
-        # Posicionando os widgets
-        self.combo_moedas.place(x=356,y=0)
-        self.destin.place(x=1,y=200)
-        self.fin.place(x=290,y=200)
-        self.cal.place(x=1, y=0)
-        self.plotar_grafico_button.place(x=200,y=22)
-        self.converss.place(x=218,y=230)
-
-
+        self.combo_moedas = ttk.Combobox(self.grafic, values=self.moedas, width=20)
+        self.combo_moedas.set('Dólar (USD)')
+        self.combo_moedas.place(x=0, y=105)
+        
+        #--------------------------------------Parte do conversor--------------------------------------
+        
+        # Receber o valor para converter fornecido pelo usuário
+        self.labb_entr = Label(self.conv, bg='#00BFFF', foreground='black', text='Valor a ser convertido:')
+        self.labb_entr.place(x=0, y=0)
+        
+        self.entrada_origem = Entry(self.conv, width=5)
+        self.entrada_origem.place(x=0, y=23)
+        
+        
+        self.labb = Label(self.conv, bg='#00BFFF', foreground='black', text='Selecione a moeda a ser convertida:')
+        self.labb.place(x=0, y=50)
  
+        self.destin = ttk.Combobox(self.conv, values=self.moedas_con, width=5)
+        self.destin.set('USD')
+        self.destin.place(x=0, y=73)
+        
+        self.labb2 = Label(self.conv, bg='#00BFFF', foreground='black', text='Selecione a moeda de destino:')
+        self.labb2.place(x=0, y=110)
+        
+        self.fin = ttk.Combobox(self.conv, values=self.moedas_con2, width=5)
+        self.fin.set('BRL')
+        self.fin.place(x=0, y=133)
+
+        # Criando o botão de conversão        
+        self.converss = Button(self.conv, bg='#808080', foreground='white', text='Converter', command=self.convo)
+        self.converss.place(x=100,y=170)
+        
+        # Criando personalização das abas         
+        self.style = ttk.Style()
+        self.style.configure('TNotebook.Tab', width=15, padding=(12, 6), tabposition='wn')
+        
+        #--------------------------------------Parte do conversor--------------------------------------
+        #Adiciona o seletor de data
+        self.calen = DateEntry(self.selic, width=12, background='darkblue', foreground='white', borderwidth=2)
+        self.calen.pack(padx=10, pady=10)
+
+        #Adiciona o botão para gerar o gráfico
+        self.gerar_botao = Button(self.selic, text="Gerar Gráfico", command=self.gerar_graafico)
+        self.gerar_botao.pack(padx=10, pady=10)
+
     def gerar_grafico(self):
 
         data_selecionada = self.cal.get_date()
@@ -151,15 +197,37 @@ class conversor_moeda:
             root.mainloop()
             
     def convo(self):
-        self.conversao = requests.get("http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,JPY-BRL,USD-BRLT")
-        #Pega as moedas disponíveis no site (Atualizado a cada 30 segundos) e armazena
+        valor = float(self.entrada_origem.get())
+        moeda_origem = self.destin.get()
+        moeda_destino = self.fin.get()
+        resultado = conversor.convert(moeda_origem, moeda_destino, valor)
+        texto_resultado = StringVar()
+        texto_resultado.set(f'{valor:.2f} {moeda_origem} equivalem a {resultado:.2f} {moeda_destino}')
+        rotulo_resultado = Label(self.conv, textvariable=texto_resultado)
+        rotulo_resultado.place(x=1,y=260)
+    
+    def gerar_graafico(self):
+        # Recupera a data selecionada do seletor de data
+        self.data_selecionada = self.calen.get_date()
+
+        #Formata a data no formato dd/mm/yyyy
+        self.data_formatada = self.data_selecionada.strftime('%d/%m/%Y')
+
+        #Carrega os dados da SELIC a partir da data selecionada até a última data disponível
+        self.serie = CarregaSELIC(self.data_formatada, "28/02/2023")
+        self.serie1 = self.serie.replace({',':'.'}, regex=True)
+        self.serie1['valor'] = self.serie1['valor'].astype(float)
+        self.serie1['data'] = pd.to_datetime(self.serie1['data'], format='%d/%m/%Y')
+        self.serie1['ano_mes'] = self.serie1['data'].dt.strftime('%Y-%m')
+
+        #Plota o gráfico da SELIC
+        self.serie1.plot.line(x="ano_mes",y="valor",title="SELIC taxas ao ano",legend=False)
+        plt.show()
         
-        self.requisicao = self.conversao.json()
-        
-        self.cotacao_dolar = self.requisicao["USDBRL"]["bid"]
-        self.cotacao_euro = self.requisicao["EURBRL"]["bid"]
-        self.cotacao_libra = self.requisicao["GBPBRL"]["bid"]
-        self.cotacao_dolcad = self.requisicao["CADBRL"]["bid"]
+def CarregaSELIC(data_inicial,data_final):
+    url_bcb = f"http://api.bcb.gov.br/dados/serie/bcdata.sgs.4189/dados?formato=csv&dataInicial={data_inicial}&dataFinal={data_final}"
+    serie_SELIC = pd.read_csv(url_bcb, sep=";")
+    return serie_SELIC
 
 master = Tk()
 janela = conversor_moeda(master)
